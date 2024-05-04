04a0aaeacf39418c840f25332819ac07f71b2198
Hmazah <alhamza2012@gmail.com>
2024-05-04 10:00:00 +0300

    add gsap repo

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+<<<<<<< HEAD
+=======
+
+>>>>>>> b6d1206 (add gsap repo)
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
