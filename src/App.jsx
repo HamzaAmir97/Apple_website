34fe1cd35b94e9548fea562998a1e48fef1496ea
Hmazah <alhamza2012@gmail.com>
2024-05-05 10:00:00 +0300

    add gsap repo

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <main className='bg-black'>
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <HowItWorks />
+      <VideoCarousel />
+    </main>
+  )
+}
+
+export default App;
