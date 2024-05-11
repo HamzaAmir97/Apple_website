2089c106f314b91240e1d1439b86c4b7623d4845
Hmazah <alhamza2012@gmail.com>
2024-05-11 10:00:00 +0300

    add gsap repo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
-// import appleImg from '../utils';
-// import searchImg from '../utils';
-// import bagtImg from '../utils';
-// import { navLists  } from '../constants';
+import{appleImg,searchImg,bagtImg} from '../utils';
+
+import { navLists  } from '../constants';
 
 const Navbar = () => {
   return (
     <header  className=' w-full py-5 sm:py-10 px-5 flex justify-between items-center '>
-        {/* <nav  className='flex w-full  screen-max-width '>
+        <nav  className='flex w-full  screen-max-width '>
            
             <img src={appleImg} alt='apple' width={18} height={30} className='w-10 h-10' />
         
         <div className='flex flex-1 justify-center  max-sm:hidden'>
-          {navLists.map((item,index)=>(
-            <div key={item} >
-              <nav>{item}</nav>S
+          {navLists.map((nav)=>(
+            <div className=' px-5 py-2.5 text-sm cursor-pointer text-gray hover:text-white transition-all  ' key={nav} >
+              <nav>{nav}</nav>S
             </div>
           ))}
 
@@ -29,7 +28,7 @@ const Navbar = () => {
 
 </div>
 
-        </nav> */}
+        </nav>
     </header>
   )
 }
