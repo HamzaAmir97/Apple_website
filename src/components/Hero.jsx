6570722d4c9a29b0dbdc6dd184adbc9b02869fd7
Hmazah <alhamza2012@gmail.com>
2024-05-15 10:00:00 +0300

    add gsap repo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import  {gsap} from 'gsap'
+import { useGSAP } from '@gsap/react'
+
 
 export const Hero = () => {
+  
+ useGSAP(()=>{
+
+  gsap.To('#hero',{
+    opacity:1,
+    y:100,}
+    
+    
+    ,[])
+  })
+  
   return (
-    <div>Hero</div>
+
+
+    <section className='w-full h-screen nav-height bg-black
+      relative'>
+    <div className='h-5/6 w-full flex-center flex-col '>
+    
+    
+   <p className='hero-title text-2xl font-bold'>
+    iphone 15 pro
+   </p>
+   
+    </div>   
+    </section>
   )
 }
 
